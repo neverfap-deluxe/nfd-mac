b7e75ec76996d2524b450fdaf65f50b0b326cb7d
Julius Reade <julius.reade@fmclarity.com>
2019-07-29 09:59:05 +1000

    yoles

---
+++ nfd-mac/Model/NFDAudio.swift
@@ -0,0 +1,13 @@
+//
+//  NFDAudio.swift
+//  nfd-mac
+//
+//  Created by Julius Reade on 29/7/19.
+//  Copyright © 2019 Julius Reade. All rights reserved.
+//
+
+import Cocoa
+
+class NFDAudio: NSObject {
+
+}
---
+++ nfd-mac/Model/NFDText.swift
@@ -0,0 +1,13 @@
+//
+//  NFDText.swift
+//  nfd-mac
+//
+//  Created by Julius Reade on 29/7/19.
+//  Copyright © 2019 Julius Reade. All rights reserved.
+//
+
+import Cocoa
+
+class NFDText: NSObject {
+
+}
